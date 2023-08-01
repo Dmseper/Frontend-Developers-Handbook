24e6ce399761073468604ae80da059de01ab6a06
Dmitry_Perepelkin <dmitry_perepelkin@relex.ru>
2023-08-01 14:49:33 +0300

    update patterns

---
+++ patterns/сreational-patterns/03_factory_method_#/factory_method_for_delivery.ts
@@ -0,0 +1,65 @@
+// Интерфейс для логистического транспортного средства
+interface Transport {
+    deliver(): void;
+}
+
+// Конкретные классы логистических транспортных средств
+class Truck implements Transport {
+    public deliver(): void {
+        console.log("Наземная доставка");
+    }
+}
+
+class Ship implements Transport {
+    public deliver(): void {
+        console.log("Морская доставка");
+    }
+}
+
+class Airplane implements Transport {
+    public deliver(): void {
+        console.log("Воздушная доставка");
+    }
+}
+
+// Фабричный метод
+abstract class LogisticsCompany {
+    public abstract createTransport(): Transport;
+
+    public deliver(): void {
+        const transport = this.createTransport();
+        transport.deliver();
+    }
+}
+
+// Конкретные фабрики для различных типов доставки
+class RoadLogistics extends LogisticsCompany {
+    public createTransport(): Transport {
+        return new Truck();
+    }
+}
+
+class SeaLogistics extends LogisticsCompany {
+    public createTransport(): Transport {
+        return new Ship();
+    }
+}
+
+class AirLogistics extends LogisticsCompany {
+    public createTransport(): Transport {
+        return new Airplane();
+    }
+}
+
+// Использование
+const roadLogistics = new RoadLogistics();
+roadLogistics.deliver();
+// Output: "Наземная доставка"
+
+const seaLogistics = new SeaLogistics();
+seaLogistics.deliver();
+// Output: "Морская доставка"
+
+const airLogistics = new AirLogistics();
+airLogistics.deliver();
+// Output: "Воздушная доставка"
